61db75fa72111893398bc2cb215fd7deb29a27ee
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-09-23 18:02:56 +0200

    porting project initializer

---
+++ kdg/rl/learning/tabular/qlearning.py
@@ -0,0 +1,32 @@
+from be.kdg.rl.agent.episode import Episode
+from be.kdg.rl.environment.environment import Environment
+from be.kdg.rl.learning.tabular.tabular_learning import TabularLearner
+
+
+class Qlearning(TabularLearner):
+
+    def __init__(self, environment: Environment, α=0.7, λ=0.0005, γ=0.9, t_max=99) -> None:
+        TabularLearner.__init__(self, environment, α, λ, γ, t_max)
+
+    def learn(self, episode: Episode):
+        # TODO: COMPLETE THE CODE
+
+        super().learn(episode)
+
+
+class NStepQlearning(TabularLearner):
+
+    def __init__(self, environment: Environment, n: int, α=0.7, λ=0.0005, γ=0.9, t_max=99) -> None:
+        TabularLearner.__init__(self, environment, α, λ, γ, t_max)
+        self.n = n  # maximum number of percepts before learning kicks in
+        self.percepts = []  # this will buffer the percepts
+
+    def learn(self, episode: Episode):
+        # TODO: COMPLETE THE CODE
+
+        super().learn(episode)
+
+
+class MonteCarloLearning:
+    # TODO: COMPLETE THE CODE
+    pass
---
+++ kdg/rl/learning/tabular/tabular_learning.py
@@ -0,0 +1,54 @@
+from abc import abstractmethod
+
+import numpy as np
+from numpy import ndarray
+
+from be.kdg.rl.agent.episode import Episode
+from be.kdg.rl.environment.environment import Environment
+from be.kdg.rl.learning.learningstrategy import LearningStrategy
+
+
+class TabularLearner(LearningStrategy):
+    """
+    A tabular learner implements a tabular method such as Q-Learning, N-step Q-Learning, ...
+    """
+    π: ndarray
+    v_values: ndarray
+    q_values: ndarray
+
+    def __init__(self, environment: Environment, α=0.7, λ=0.0005, γ=0.9, t_max=99) -> None:
+        super().__init__(environment, λ, γ, t_max)
+        # learning rate
+        self.α = α
+
+        # policy table
+        self.π = np.full((self.env.n_actions, self.env.state_size), fill_value=1 / self.env.n_actions)
+
+        # state value table
+        self.v_values = np.zeros((self.env.state_size,))
+
+        # state-action table
+        self.q_values = np.zeros((self.env.state_size, self.env.n_actions))
+
+    @abstractmethod
+    def learn(self, episode: Episode):
+        # subclasses insert their implementation at this point
+        # see for example be\kdg\rl\learning\tabular\qlearning.py
+        self.evaluate()
+        self.improve()
+        super().learn(episode)
+
+    def on_episode_start(self):
+        self.t = 0
+
+    def next_action(self, s: int):
+        # TODO: COMPLETE THE CODE, REPLACE THE LINE BELOW
+        return self.env.action_space.sample()
+
+    def evaluate(self):
+        # TODO: COMPLETE THE CODE
+        pass
+
+    def improve(self):
+        # TODO: COMPLETE THE CODE
+        pass
